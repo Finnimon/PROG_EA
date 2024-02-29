3114389e4a166ad9dd9a0bfb5060ca9c40564ffd
Finn Lindig <Lindig@swms.de>
2024-02-29 11:31:39 +0100

    Translated, commented and implemented TreeCadastreQueryController

--- src/model/LinearFunction.java
+++ src/model/LinearFunction.java
@@ -1,24 +1,24 @@
 package model;
 
 /**
+ * @param slope
+ * @param yIntersect
  * @Summary: This record represents the mathematical concept linear function.
  * @Author: Finn Lindig
  * @Since: 26.02.2024
- * @param slope
- * @param yIntersect
  */
 public record LinearFunction(float slope, float yIntersect)
 {
     
     
     /**
+     * @param x the x value
+     * @return y=slope*x+yIntersect
      * @Precondition: slope != float.nan
      * @Postcondition: f(x)!=float.nan
      * @Summary: f(x) returns the result of f(x), y.
      * @Author: Finn Lindig
      * @Since: 26.02.2024
-     * @param x the x value
-     * @return y=slope*x+yIntersect
      */
     public float f(float x)
     {
@@ -27,14 +27,14 @@ public record LinearFunction(float slope, float yIntersect)
     
     
     /**
+     * @param y the y value
+     * @return f^(-1)(y)=x
      * @Precondition: slope != 0
      * @Postcondition: f^(-1)(y)!=float.nan. Der return von fVonX(InverseFVonY(x))==y.
      * @Summary: inverse returns f^(-1)(y)=x.
      * If f(x) is not invertible it will return float.nan.
      * @Author: Finn Lindig
      * @Since: 26.02.2024
-     * @param y the y value
-     * @return f^(-1)(y)=x
      */
     public float inverse(float y)
     {
